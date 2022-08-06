71ecc0493201aa3e500d214a0b21e347f17c834d
abhinavxox <abhinavxox@gmail.com>
2022-08-06 09:14:59 +0545

    <array>:[Max area of container]

---
+++ 30DayDsaChallenge/Medium/ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package Medium;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int n = height.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // to reduce complexity
+                if (j > i) {
+                    // finding the area
+                    int firstHeight = height[i];
+                    int secondHeight = height[j];
+                    int area;
+                    if (firstHeight < secondHeight) {
+                        area = firstHeight * (j - i);
+                    } else {
+                        area = secondHeight * (j - i);
+                    }
+                    // finding the max
+                    if (area > maxArea) {
+                        maxArea = area;
+                    }
+                }
+            }
+        }
+        return maxArea;
+    }
+}
