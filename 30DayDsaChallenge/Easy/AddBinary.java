8c2e00ca0aaffb790460a5a9eae8d36f02a8d8ac
abhinavxox <abhinavxox@gmail.com>
2022-08-10 21:16:44 +0545

    <easy>:[add binary numbers]

---
+++ 30DayDsaChallenge/Easy/AddBinary.java
@@ -0,0 +1,28 @@
+package Easy;
+
+public class AddBinary {
+
+    public static void main(String[] args) {
+        String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+        System.out.println(addBinary(a, b));
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0)
+                sum += b.charAt(j--) - '0';
+            if (i >= 0)
+                sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0)
+            sb.append(carry);
+        return sb.reverse().toString();
+
+    }
+}
