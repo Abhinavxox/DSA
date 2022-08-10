78fd580e978f82ed03b4f5bdd294a01dce611c81
abhinavxox <abhinavxox@gmail.com>
2022-08-10 16:18:13 +0545

    <easy>:[length of the last word]

---
+++ 30DayDsaChallenge/Easy/LengthOfLastWord.java
@@ -0,0 +1,8 @@
+package Easy;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String arr[] = s.split(" ");
+        return arr[arr.length - 1].length();
+    }
+}
