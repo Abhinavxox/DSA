55e054604e1e24a3b36f97a93c3a094e78e67ff5
abhinavxox <abhinavxox@gmail.com>
2022-08-20 20:50:31 +0545

    <easy>:[find the single number in array]

---
+++ 30DayDsaChallenge/Easy/SingleNumber.java
@@ -0,0 +1,24 @@
+package Easy;
+
+import java.util.ArrayList;
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 2, 1 };
+        System.out.println(singleNumber(nums));
+        ;
+    }
+
+    public static int singleNumber(int[] nums) {
+        ArrayList<Integer> alist = new ArrayList<>();
+        for (int x : nums) {
+            if (!alist.contains(x)) {
+                alist.add(x);
+            } else {
+                alist.remove(alist.indexOf(x));
+            }
+        }
+        return alist.get(0);
+    }
+}
