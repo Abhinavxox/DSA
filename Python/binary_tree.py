1fd0bcde9959297251a5ec41e5b2addd386c577a
abhinavxox <abhinavxox@gmail.com>
2022-09-29 11:18:20 +0545

    <binary tree>:[implemented creation of binary tree in python]

---
+++ Python/binary_tree.py
@@ -0,0 +1,41 @@
+class Node:
+    
+    #initialization 
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        #if tree is not empty
+        if self.data:
+            if data < self.data:
+                if self.left == None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right == None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        #if tree is empty
+        else:
+            self.data = data
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
+
+#creating and inserting nodes
+root = Node(69)
+root.insert(70)
+root.insert(13)
+root.insert(45)
+root.insert(23)
+root.insert(12)
+root.print_tree()
+
