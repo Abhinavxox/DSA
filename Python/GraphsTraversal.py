2d5be3d5b5c153c6e290d47a5b4e5a552664de12
Abhinavxox <abhinavxox@gmail.com>
2023-01-30 12:02:52 +0530

    <>:[]

--- Python/GraphsTraversal.py
+++ Python/GraphsTraversal.py
@@ -2,7 +2,6 @@
 #     def __init__(self, n):
 #         self.name = n
 #         self.neighbors = list()
-#         self.color = 'black'
 
 #     def add_neighbor(self, v):
 #         if v not in self.neighbors:
